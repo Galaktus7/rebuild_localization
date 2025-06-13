c5ff7d0bbfc8fde8b90ca1601448a443da291eac
Galaktus <54316412+Galaktus7@users.noreply.github.com>
2025-06-13 22:13:10 +0500

    Update environ.py

--- environ.py
+++ environ.py
@@ -1,7 +1,7 @@
 environ = {
-'database': '',
+'database': 'mongodb://mongo:WCeMDpPKUniDbZONlwnitbUQmAxqlrdZ@nozomi.proxy.rlwy.net:43163',
 'token': '7761726947:AAGOpSlfreC-GPanhCb89Vs5hS4vafxV0RQ',
 'rhinomaxloss': '2',
 'rhinominloss': '1',
-'database2': ''
+'database2': 'mongodb://mongo:WCeMDpPKUniDbZONlwnitbUQmAxqlrdZ@nozomi.proxy.rlwy.net:43163'
 }
